88f9df3bf28ab1cd3c605451c72087c61a05ebb9
Rickey Olsen <olsenrickey@gmail.com>
2022-06-28 18:09:14 -0500

    Day 8 update
    Made the script loop through until user indicated they were done. Fixed for when shift is out of range of the index for alphabet.

--- Day 8/caesar.py
+++ Day 8/caesar.py
@@ -4,18 +4,31 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 print(logo)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def caesar(old_text, shift_amount):
     new_text = ""
     if direction == "decode":
         shift_amount *= -1
     for letter in old_text:
-        x = alphabet.index(letter)
-        y = x + shift_amount
-        new_text += alphabet[y]
+        if letter not in alphabet:
+            new_text += letter
+        else:
+            x = alphabet.index(letter)
+            y = x + shift_amount
+            new_text += alphabet[y]
     print(f"Here's the {direction}d result: {new_text}")
 
-caesar(old_text=text, shift_amount=shift)
+play = True
+while play:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caesar(old_text=text, shift_amount=shift)
+
+    play_again = input("Type 'yes' if you want to run again. Otherwise type 'no'. ")
+    
+    if play_again == "no":
+        play = False
+        print("Goodbye!")
